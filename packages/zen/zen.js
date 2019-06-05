66b214e6c044a9c3f64d0ff60f6be4d244ca99cb
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-06-05 15:26:32 +0300

    Add workaround to run zen in yarn v1 projects in yarn linked mode

--- packages/zen/zen.js
+++ packages/zen/zen.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
-require('source-map-support').install();
+const path = require('path');
 
+// If we run in yarn linked mode, require .pnp manually
+path.basename(path.dirname(__dirname)) === 'packages' && require('../../.pnp').setup();
+
+require('source-map-support').install();
 require('./lib/cli');
