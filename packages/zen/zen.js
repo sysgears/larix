a05ee8f556d0fd7c38d403d8dc86763e89990fb4
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-09-09 22:15:05 +0300

    More yarn v2 specific workarounds for publishing

--- packages/zen/zen.js
+++ packages/zen/zen.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
-const path = require('path');
-
-// If we run in yarn linked mode, require .pnp manually
-path.basename(path.dirname(__dirname)) === 'packages' && require('../../.pnp').setup();
-
 require('source-map-support').install();
 require('./lib/cli');
